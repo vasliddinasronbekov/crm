6e01d0811ff60db3ae031bf574e5c343d3c14f00
Windows User <asronbekovvasliddin@gmail.com>
2026-03-05 05:33:45 -0500

    .

--- backend/edu_project/asgi.py
+++ backend/edu_project/asgi.py
@@ -1,21 +1,26 @@
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edu_project.settings")
+
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
+from edu_project.middleware.ws_auth import TokenAuthMiddleware
 import ai.routing
 import messaging.routing
 import student_profile.routing
-from edu_project.middleware.ws_auth import TokenAuthMiddleware
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edu_project.settings")
-django_asgi_app = get_asgi_application()
+websocket_urlpatterns = (
+    ai.routing.websocket_urlpatterns
+    + messaging.routing.websocket_urlpatterns
+    + student_profile.routing.websocket_urlpatterns
+)
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": TokenAuthMiddleware(
-        URLRouter(
-            ai.routing.websocket_urlpatterns
-            + messaging.routing.websocket_urlpatterns
-            + student_profile.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": TokenAuthMiddleware(URLRouter(websocket_urlpatterns)),
+    }
+)
